9b7bb32a0e44566966b07a7e0aa2b561340c8e95
Murilo Miranda <biomurilodantas@gmail.com>
2018-03-04 00:57:49 +0100

    Gridmaker works

--- 14pixel-art-maker/designs.js
+++ 14pixel-art-maker/designs.js
@@ -4,18 +4,34 @@ function makeGrid(height, weight) {
   for(let i=0; i<height; i++){
     const row = table.insertRow(i);
     for(let j=0; j<weight; j++){
-      row.insertCell(j).innerHTML = i+","+j;
+      row.insertCell(j);  //.innerHTML = i+","+j;
     }
   }
+
+  // var tbody = '';
+  // for(let i=0; i<height; i++){
+  //   tbody += '<tr>';
+  //   for(let j=0; j<weight; j++){
+  //     tbody += '<td>';
+  //     tbody += 'Cell ' + i + ',' + j;
+  //     tbody += '</td>'
+  //   }
+  //   tbody += '</tr>\n';
+  // }
+  // document.getElementById("pixelCanvas").innerHTML = tbody;
 }
 
 // Select color input
 let color = document.querySelector("#colorPicker").value;
 
 // Select size input
+var btn = document.querySelector('#submit');
+
+btn.addEventListener('click', sizeInput);
+
 function sizeInput() {
     const height = document.getElementById("inputHeight").value;
     const weight = document.getElementById("inputWeight").value;
-
+    // alert(height +","+ weight);
     makeGrid(height, weight);
 }
