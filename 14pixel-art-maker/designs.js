3611d9730049ccebe2c9e0d16e662adcab9a719a
Murilo Miranda <biomurilodantas@gmail.com>
2018-03-03 23:23:33 +0100

    Getting weight and height

--- 14pixel-art-maker/designs.js
+++ 14pixel-art-maker/designs.js
@@ -1,5 +1,11 @@
 // Select color input
+
 // Select size input
+function sizeInput() {
+    const height = document.getElementById("inputHeight").value;
+    const weight = document.getElementById("inputWeight").value;
+    alert("height: " + height + ", weight: " + weight);
+}
 
 // When size is submitted by the user, call makeGrid()
 
