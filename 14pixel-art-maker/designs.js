b00bc905a4d7d8ffeb2d6b04f6fedee880c9df16
Murilo Miranda <biomurilodantas@gmail.com>
2018-03-04 14:31:02 +0100

    Add Event Listener to Cells

--- 14pixel-art-maker/designs.js
+++ 14pixel-art-maker/designs.js
@@ -1,37 +1,48 @@
-// When size is submitted by the user, call makeGrid()
-function makeGrid(height, weight) {
-  const table = document.getElementById("pixelCanvas");
-  for(let i=0; i<height; i++){
-    const row = table.insertRow(i);
-    for(let j=0; j<weight; j++){
-      row.insertCell(j);  //.innerHTML = i+","+j;
-    }
-  }
-
-  // var tbody = '';
-  // for(let i=0; i<height; i++){
-  //   tbody += '<tr>';
-  //   for(let j=0; j<weight; j++){
-  //     tbody += '<td>';
-  //     tbody += 'Cell ' + i + ',' + j;
-  //     tbody += '</td>'
-  //   }
-  //   tbody += '</tr>\n';
-  // }
-  // document.getElementById("pixelCanvas").innerHTML = tbody;
-}
-
 // Select color input
 let color = document.querySelector("#colorPicker").value;
 
 // Select size input
 var btn = document.querySelector('#submit');
-
 btn.addEventListener('click', sizeInput);
 
 function sizeInput() {
     const height = document.getElementById("inputHeight").value;
     const weight = document.getElementById("inputWeight").value;
     // alert(height +","+ weight);
+
+    const bodyTable = document.querySelector("#pixelCanvas").firstElementChild;
+    if(bodyTable !== null){
+      bodyTable.remove();
+    }
     makeGrid(height, weight);
+}
+
+// When size is submitted by the user, call makeGrid()
+function respondToClick(evt){
+  console.log(evt);
+}
+
+function makeGrid(height, weight) {
+  const table = document.getElementById("pixelCanvas");
+  for(let i=0; i<height; i++){
+    const row = table.insertRow(i);
+    for(let j=0; j<weight; j++){
+      var row_col = row.insertCell(j);  //.innerHTML = i+","+j;
+      //row_col.colSpan = j;
+      //row_col.rowSpan = i;
+      //row_col.innerHTML = i+","+j;
+    }
+  }
+  // var tbody = '';
+  // for(let i=0; i<height; i++){
+  //   tbody += '<tr>';
+  //   for(let j=0; j<weight; j++){
+  //     tbody += '<td>';
+  //     //tbody += 'Cell ' + i + ',' + j;
+  //     tbody += '</td>'
+  //   }
+  //   tbody += '</tr>\n';
+  // }
+  // document.getElementById("pixelCanvas").innerHTML = tbody;
+  table.addEventListener('click', respondToClick);
 }
